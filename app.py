6fc021f7f9153f1826bc1f88124ffa65a106b3ed
vitkanurazizah <nurazizahvitka21@gmail.com>
2026-05-23 10:40:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,22 @@ st.write("Hello, *im mermaid!* :🧜🏻:")
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
 
-if st.button("hey r u still there"):
+if st.button("hey r u still there?"):
     st.write("good")
 else:
     st.write("Goodbye")
 
 
 st.button("Reset", type="primary")
+
+
+from numpy.random import default_rng as rng
+
+df = rng(0).standard_normal((10, 1))
+col1, col2 = st.columns([3, 1])
+
+col1.subheader("A wide column with a chart")
+col1.line_chart(df)
+
+col2.subheader("A narrow column with the data")
+col2.write(df)
