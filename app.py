19658c9461579656aa9c531904ec74d750b587b3
vitkanurazizah <nurazizahvitka21@gmail.com>
2026-05-23 10:48:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,3 +39,13 @@ with col2:
 with col3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg")
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
