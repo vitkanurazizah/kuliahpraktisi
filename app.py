e61fd7e8bd7115b32cdfe62fd884dc577bf25d3e
vitkanurazizah <nurazizahvitka21@gmail.com>
2026-05-23 10:27:18 +0700

    Update greeting and add movie title input

--- app.py
+++ app.py
@@ -12,4 +12,7 @@ st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
-st.write("Hello, *World!* :🧜🏻:")
+st.write("Hello, *im mermaid!* :🧜🏻:")
+
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
