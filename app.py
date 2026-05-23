6008ba6ba291d729589ba77f4238956bac8dc389
vitkanurazizah <nurazizahvitka21@gmail.com>
2026-05-23 11:47:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title("viezzh")
 st.header("when ya")
 st.subheader("when ya")
 st.caption("how r u?")
-st.write("Hello, *im mermaid!* :🧜🏻:")
+st.write("Hello, *backburner* :🫢:")
 
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
@@ -28,17 +28,6 @@ st.button("Reset", type="primary")
 
 col1, col2, col3 = st.columns(3)
 
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
 
 st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
 
